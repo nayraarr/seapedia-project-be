c1e695069d1ddd81feab5557de2df3d6a9581f1a
nayraarr <zita.nayra.ardini@gmail.com>
2026-06-20 16:54:26 +0700

    Initial project.

---
+++ src/main/java/id/seapedia/seapediaprojectbe/SeapediaProjectBeApplication.java
@@ -0,0 +1,13 @@
+package id.seapedia.seapediaprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeapediaProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeapediaProjectBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/seapedia/seapediaprojectbe/SeapediaProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.seapedia.seapediaprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeapediaProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
