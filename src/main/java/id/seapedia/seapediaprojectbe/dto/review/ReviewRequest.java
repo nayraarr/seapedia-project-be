20769677178959dbbfe7a550890457ada4247b50
nayraarr <zita.nayra.ardini@gmail.com>
2026-06-21 23:29:40 +0700

    Implement level 1 subpoint 1.

--- src/main/java/id/seapedia/seapediaprojectbe/dto/review/ReviewRequest.java
+++ src/main/java/id/seapedia/seapediaprojectbe/dto/review/ReviewRequest.java
@@ -1,4 +1,18 @@
 package id.seapedia.seapediaprojectbe.dto.review;
 
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data @NoArgsConstructor @AllArgsConstructor
 public class ReviewRequest {
+    @NotBlank(message = "Reviewer name is required")
+    private String reviewerName;
+
+    @NotNull(message = "Rating is required")
+    @Min(value = 1, message = "Rating minimum 1")
+    @Max(value = 5, message = "Rating maximum 5")
+    private Integer rating;
+
+    @NotBlank(message = "Comment is required")
+    private String comment;
 }
--- src/main/java/id/seapedia/seapediaprojectbe/service/ProductService.java
+++ src/main/java/id/seapedia/seapediaprojectbe/service/ProductService.java
@@ -1,4 +1,11 @@
 package id.seapedia.seapediaprojectbe.service;
 
-public class ProductService {
+import id.seapedia.seapediaprojectbe.dto.product.ProductResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+    List<ProductResponse> getAllProducts();
+    ProductResponse getProductById(UUID id);
 }
