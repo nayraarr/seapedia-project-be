44e4cf235b237e008128802f5168d0b91db88d23
nayraarr <zita.nayra.ardini@gmail.com>
2026-06-24 23:35:04 +0700

    Fix escaping method.

--- src/main/java/id/seapedia/seapediaprojectbe/util/SanitizerUtil.java
+++ src/main/java/id/seapedia/seapediaprojectbe/util/SanitizerUtil.java
@@ -1,13 +1,11 @@
 package id.seapedia.seapediaprojectbe.util;
 
-import org.springframework.web.util.HtmlUtils;
-
 public final class SanitizerUtil {
 
     private SanitizerUtil() {}
 
     public static String clean(String input) {
         if (input == null) return null;
-        return HtmlUtils.htmlEscape(input.trim());
+        return input.trim();
     }
 }
